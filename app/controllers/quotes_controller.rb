784e7f6a3806680e39d13c33ff91757323b3e2d9
宇治祐介 <ujiyusuke@ujiyuusuke-no-MacBook-Pro.local>
2017-05-11 16:53:44 +0900

    login

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,53 +1,54 @@
 class QuotesController < ApplicationController
+   before_action :authenticate_user!
    before_action :set_quotes, only: [:edit, :update, :destroy]
-  
+
   def index
     @quotes=Quote.all
   end
-  
+
   def new
 if params[:back]
 @quotes=Quote.new(quotes_params)
 else
-  @quotes=Quote.new 
+  @quotes=Quote.new
 end
   end
-  
-  
+
+
   def create
     @quotes=Quote.new(quotes_params)
     if @quotes.save
-    
+
     redirect_to quotes_path,notice:"Your favorite quote has posted！"
-    
+
   else
     render 'new'
   end
   end
-  
+
   def confirm
     @quotes=Quote.new(quotes_params)
     render :new if @quotes.invalid?
   end
-  
+
   def edit
   end
-  
+
   def update
     if @quotes.update(quotes_params)
     redirect_to quotes_path,notice:'Updated '
-  else 
+  else
     render'edit'
   end
 end
 
   def destroy
     @quotes.destroy
-    
+
     redirect_to quotes_path
   end
-  
-  
+
+
   private
     def quotes_params
       params.require(:quote).permit(:content)
